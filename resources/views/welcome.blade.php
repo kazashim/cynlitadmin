58c5597d90726e0e7c0d1866c1ddf6082620b95e
Kazashim <kazashimkuzasuwat@gmail.com>
2019-07-22 22:11:36 +0200

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,17 +81,17 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    CynAdmin
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://Cynojine.com/docs">Docs</a>
+                    <a href="https://cynojine.com">Cyncasts</a>
+                    <a href="https://cynojine.com">News</a>
+                    <a href="https://cynojine.com">Blog</a>
+                    <a href="https://cynojine.com">Nova</a>
+                    <a href="https://cynojine.com">Forge</a>
+                    <a href="https://github.com/kazashim/cynlitadmin">GitHub</a>
                 </div>
             </div>
         </div>
